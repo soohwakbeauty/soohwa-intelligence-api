9021884b7fc5f6320010d50245e63726743028a5
Christophe <soohwa.kbeauty@gmail.com>
2026-06-19 18:38:53 +0200

    Add analyze endpoint

---
+++ api/analyze.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "Analyse reçue",
+    received: req.body
+  });
+
+}
